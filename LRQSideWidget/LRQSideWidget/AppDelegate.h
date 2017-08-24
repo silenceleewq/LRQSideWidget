532a0912543dcc33e1cfebae7bbb91d00d7b2c8b
lirenqiang <958246321@qq.com>
2017-08-24 20:36:28 +0800

    init project

---
+++ LRQSideWidget/LRQSideWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRQSideWidget
+//
+//  Created by lirenqiang on 2017/8/24.
+//  Copyright © 2017年 李仁强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRQSideWidget/LRQSideWidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRQSideWidget
+//
+//  Created by lirenqiang on 2017/8/24.
+//  Copyright © 2017年 李仁强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
